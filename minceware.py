b1a2e1adda92eec497592e733df2f96f5ab94caa
Nikola Pavlović <nzp@riseup.net>
2016-09-28 14:30:08 +0200

    Basic functionality usable from command line.
    Make it usable both as a module and as a script.  Basic functionality:
    given a word list, and desired entropy or number of words, generate a
    passphrase.

--- minceware.py
+++ minceware.py
@@ -1,24 +1,103 @@
+#! /usr/bin/env python3
+
+"""
+Minceware generates truly random Diceware style passphrases on a computer.
+
+Exported functions:
+get_length_from_entropy -- calculate passphrase length based on entropy and word list length
+generate_passphrase -- generates passphrase from a given word list
+
+For standalone usage and command line arguments run
+
+``minceware (--help|-h)``
+
+"""
+
 import sys
 import os
+import math
 
-w = 8
-words_file = '/usr/share/dict/cracklib-small'
 
-with open(words_file) as f:
-    word_list = f.readlines()
+def get_length_from_entropy(entropy, list_length):
+    """Calculate the number of words in a passphrase.
 
-passphrase = []
+    The length returned is rounded up so that we always have at least
+    ``entropy`` bits in a passphrase.
 
-while True:
-    n = int.from_bytes(os.urandom(2), sys.byteorder)
+    Arguments:
+    entropy -- desired minimum entropy of a passphrase in bits (shannons)
+    list_length -- number of words in the word list
+    """
+    return math.ceil(entropy/math.log(list_length, 2))
 
-    try:
-        passphrase.append(word_list[n].rstrip('\n'))
-    except IndexError:
-        continue
 
-    if len(passphrase) == w:
-        break
+def generate_passphrase(passphrase_length, word_list):
+    """Generate the passphrase string.
 
-print(' '.join(passphrase))
+    For each word, read 2 bytes from the kernel-space CSPRNG via ``os.urandom``
+    and convert them to an integer which serves as a list index.  A list of
+    65,536 words is probably enough so we don't need more than 2 bytes
+    (“2 bytes should be enough for everyone.”).
 
+    Return a space delimited string of words, for the same reason Diceware
+    recommends spaces to be included in the passphrase.  Additionally, spaces
+    make it much more legible.
+
+    Arguments:
+    passphrase_length -- number of words in the resulting passphrase
+    word_list -- list of words from which the passphrase is generated
+    """
+    passphrase = []
+
+    while True:
+        n = int.from_bytes(os.urandom(2), sys.byteorder)
+
+        try:
+            passphrase.append(word_list[n].rstrip('\n'))
+        except IndexError:
+            continue
+
+        if len(passphrase) == passphrase_length:
+            break
+
+    return ' '.join(passphrase)
+
+
+if __name__ == '__main__':
+    import argparse
+
+
+    parser = argparse.ArgumentParser(
+        description='Securely generate Diceware style passphrases on a computer.'
+    )
+    parser.add_argument('-f',
+                        dest='words_file',
+                        default='/usr/share/dict/cracklib-small',
+                        type=argparse.FileType('r'),
+                        help='File which contains the word list.  Default: %(default)s',
+                        metavar='PATH_TO_WORD_LIST'
+                        )
+    strength = parser.add_mutually_exclusive_group()
+    strength.add_argument('-w',
+                          dest='number_of_words',
+                          type=int,
+                          help='Number of words in the passphrase.  \
+                            Mutually exclusive with -e.')
+    strength.add_argument('-e',
+                          dest='bits',
+                          type=int,
+                          default=128,
+                          help='Minimum strength of passphrase in bits.  \
+                            Mutually exclusive with -w.  Default: %(default)s.')
+
+    args = parser.parse_args()
+
+    word_list = args.words_file.readlines()
+    args.words_file.close()
+
+    if args.number_of_words:
+        length = args.number_of_words
+    else:
+        length = get_length_from_entropy(args.bits, len(word_list))
+
+    print(generate_passphrase(length, word_list))
